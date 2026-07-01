39a87c9ae6665208156839142282b8b3a5e822c7
Huzaifa <huzaifa78678678612@gmail.com>
2026-07-01 14:08:17 +0530

    Solved Cors error

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: ["http://localhost:5173", process.env.CLIENT_URL],
     credentials: true,
   }),
 );
